5453f2607058ec4f2ca0d22d4074c902b4ffac0a
Adamus2170 <adamracki05@gmail.com>
2024-05-04 16:06:15 +0200

    yes

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -6,8 +6,7 @@ use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\Service\MixRepository;
 #use Twig\Environment;
 
 use function Symfony\Component\String\u;
@@ -32,45 +31,13 @@ class VinylController extends AbstractController
     }
 
     #[Route("/browse/{slug}", name: "app_browse")]
-    public function browse(HttpClientInterface $httpClient, CacheInterface $cache, string $slug = null): Response
+    public function browse(MixRepository $mixRepository, string $slug = null): Response
     {
-        $genre = $slug ? u(str_replace("-", " ", $slug))->title(true) : null;
-        
-        $mixes = $cache->get('mixes_data', function(CacheItemInterface $cacheItem) use ($httpClient) 
-        {
-            $cacheItem->expiresAfter(5);
-            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
-
-            return $response->toArray();
-        });
-        
-        
-        return $this->render("vinyl/browse.html.twig", [
-            "genre" => $genre,
-            "mixes" => $mixes,
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        $mixes = $mixRepository->findAll();
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre,
+            'mixes' => $mixes,
         ]);
-    }
-
-    private function getMixes(): array {
-        return [
-            [
-                'title' => 'obiwan',
-                'trackCount' => 14,
-                'genre' => 'Republic',
-                'createdAt' => new \DateTime("2021-10-02"),
-            ],
-            [
-                'title' => 'dooku',
-                'trackCount' => 8,
-                'genre' => 'Separatist',
-                'createdAt' => new \DateTime("2022-08-17"),
-            ],
-            [
-                'title' => 'jaba',
-                'trackCount' => 10,
-                'genre' => 'Huts',
-                'createdAt' => new \DateTime("2020-07-21"),
-            ],
-        ];
     }
 }
